51c880b169c9f625e774895ec1dddc2a64d1b798
Richard Trapanese <richardtrapanese@gmail.com>
2018-05-10 01:08:16 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
 # unless you shout it (type in all capitals).
   if phrase == "I LOVE YOU GRANDMA!"
      "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase
+  elsif phrase != phrase.upcase
       "HUH?! SPEAK UP, SONNY!"
     else
         "NO, NOT SINCE 1938!"
