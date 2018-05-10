9f459e913e8885d4d1211787454d4e0af55dfc63
Richard Trapanese <richardtrapanese@gmail.com>
2018-05-10 01:01:39 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,9 +6,9 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.uppercase
-    return "NO, NOT SINCE 1938!"
+    return  "HUH?! SPEAK UP, SONNY!"
     else
-      return  "HUH?! SPEAK UP, SONNY!"
+      return  "NO, NOT SINCE 1938!" 
   end
 end
 
